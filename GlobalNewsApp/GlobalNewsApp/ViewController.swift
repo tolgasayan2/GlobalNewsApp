9bb0cf05de82f67b202b1777a306e424d4b3fa82
Tolga Sayan <tolgasyn2@gmail.com>
2023-07-21 19:18:42 +0300

    [feature/globalNews][create project]

---
+++ GlobalNewsApp/GlobalNewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GlobalNewsApp
+//
+//  Created by Tolga Sayan on 21.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
