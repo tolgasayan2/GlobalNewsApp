a36a52431ba246df57119b971642fb8392c27430
Tolga Sayan <tolgasyn2@gmail.com>
2023-07-21 19:21:46 +0300

    [feature/globalNews][create Common & UIComponents modules]

---
+++ Common/Utilities/Common.h
@@ -0,0 +1,18 @@
+//
+//  Common.h
+//  Common
+//
+//  Created by Tolga Sayan on 21.07.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Common.
+FOUNDATION_EXPORT double CommonVersionNumber;
+
+//! Project version string for Common.
+FOUNDATION_EXPORT const unsigned char CommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Common/PublicHeader.h>
+
+
